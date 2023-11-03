a09d9d2a5a01f0d8d28200d287cfd15255d57c24
Lilith Camplin <lilith.camplin@gmail.com>
2023-11-03 02:12:04 +0100

    "Added descriptive comments to LateStageCapitalismSort algorithm
    Extra comments have been included for improved readability and better understanding of the code. These comments provide detailed insights into the algorithm's representation, functionality, and its overriding method, making them easily comprehensible."

--- src/main/java/eu/lilithmonodia/javasorts/sorts/impracticalsorts/LateStageCapitalismSort.java
+++ src/main/java/eu/lilithmonodia/javasorts/sorts/impracticalsorts/LateStageCapitalismSort.java
@@ -5,8 +5,19 @@ import eu.lilithmonodia.javasorts.sorts.SortingAlgorithm;
 import java.util.List;
 import java.util.Random;
 
+/**
+ * A sorting algorithm that represents the chaos and inequality of late stage capitalism.
+ * It randomly shuffles the elements in the list, simulating the unpredictability and unfairness of the economy.
+ * <p>
+ * This algorithm extends the abstract class SortingAlgorithm and provides the implementation for the sort method.
+ */
 public class LateStageCapitalismSort extends SortingAlgorithm {
 
+    /**
+     * Sorts a list of integers in ascending order.
+     *
+     * @param list the list of integers to be sorted
+     */
     @Override
     public void sort(List<Integer> list) {
         final Random rand = new Random();
