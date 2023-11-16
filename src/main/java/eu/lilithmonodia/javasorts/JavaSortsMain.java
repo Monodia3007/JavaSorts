0e7b5c61fa16e32c0a6c506d49b208a1fe1903d9
Lilith Camplin <lilith.camplin@gmail.com>
2023-11-16 19:08:56 +0100

    Refactor sorting process and enhance exception logging
    Reorganized code to streamline the sorting process and improve readability. The changes involve restructuring of task submission and handling. Rather than creating a copy of the list inside the executor submit lambda, it is prepared upfront before task submission, improving clarity. Additionally, methods related to logging and exception handling have been also expanded for easier debugging and tracing.

--- src/main/java/eu/lilithmonodia/javasorts/JavaSortsMain.java
+++ src/main/java/eu/lilithmonodia/javasorts/JavaSortsMain.java
@@ -89,18 +89,13 @@ public class JavaSortsMain {
      */
     private static void performAndLogSortingTasks(List<Integer> list, String @NotNull [] algorithmNames, SortingAlgorithmFactory sortingAlgorithmFactory) {
         ExecutorService executor = Executors.newFixedThreadPool(algorithmNames.length);
-
         List<Future<?>> futures = new ArrayList<>();
         for (String name : algorithmNames) {
-            Future<?> future = executor.submit(() -> {
-                List<Integer> listCopy = new ArrayList<>(list);
-                sortAndLog(listCopy, name, sortingAlgorithmFactory);
-            });
+            Future<?> future = executor.submit(() -> sortAndLog(list, name, sortingAlgorithmFactory));
             futures.add(future);
         }
-
         executor.shutdown();
-        waitForTasksCompletion(futures);
+        handleFutures(futures);
         LOGGER.log(Level.INFO, "All tasks have completed execution.");
     }
 
@@ -112,32 +107,9 @@ public class JavaSortsMain {
      * @param sortingAlgorithmFactory the factory object used to create the sorting algorithm
      */
     private static void sortAndLog(List<Integer> list, String name, @NotNull SortingAlgorithmFactory sortingAlgorithmFactory) {
-        StringBuilder outputSb = new StringBuilder();
+        List<Integer> listCopy = new ArrayList<>(list);
         SortingAlgorithm algorithm = sortingAlgorithmFactory.getSortingAlgorithm(name);
-
-        ExecutorService executor = Executors.newSingleThreadExecutor();
-        Future<?> future = executor.submit(() -> algorithm.displayAndTime(list, name, outputSb));
-
-        try {
-            future.get(JavaSortsMain.TIMEOUT, JavaSortsMain.UNIT);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            LOGGER.log(Level.SEVERE, "The execution of the {0} algorithm was interrupted or failed.", new Object[]{name});
-        } catch (ExecutionException e) {
-            LOGGER.log(Level.SEVERE, "The execution of the {0} algorithm was interrupted or failed.", new Object[]{name});
-        } catch (TimeoutException e) {
-            LOGGER.log(Level.SEVERE, "The execution of the {0} algorithm exceeded the time limit.", new Object[]{name});
-        } finally {
-            executor.shutdown();
-        }
-
-        String formattedDuration = extractTimeFromOutput(outputSb.toString());
-
-        if (!"Time not available".equals(formattedDuration)) {
-            LOGGER.log(Level.INFO, "Sorting with {0} algorithm on list of size {1} took {2}",
-                    new Object[]{name, list.size(), formattedDuration});
-            DB_LOGGER.addLog(formattedDuration, list.size(), name);
-        }
+        executeSortingTask(listCopy, name, algorithm);
     }
 
     /**
@@ -154,22 +126,84 @@ public class JavaSortsMain {
     }
 
     /**
-     * Waits for the completion of a list of Future tasks.
+     * Executes a sorting task using the specified algorithm and waits for its completion.
      *
-     * @param futures the list of Future tasks to wait for completion
+     * @param listCopy   the list to be sorted
+     * @param name       the name of the sorting task
+     * @param algorithm  the sorting algorithm to be used
      */
-    private static void waitForTasksCompletion(@NotNull List<Future<?>> futures) {
+    private static void executeSortingTask(List<Integer> listCopy, String name, SortingAlgorithm algorithm) {
+        StringBuilder outputSb = new StringBuilder();
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Future<?> future = executor.submit(() -> algorithm.displayAndTime(listCopy, name, outputSb));
+        handleSingleFuture(future, name, listCopy, outputSb, executor);
+    }
+
+    /**
+     * Handles the completion of a single Future task.
+     *
+     * @param future    the Future task to handle
+     * @param name      the name of the task
+     * @param listCopy  the copy of the list used in the task
+     * @param outputSb  the StringBuilder used to store the task output
+     * @param executor  the ExecutorService used to execute the task
+     */
+    private static void handleSingleFuture(Future<?> future, String name, List<Integer> listCopy, StringBuilder outputSb, ExecutorService executor) {
+        try {
+            future.get(JavaSortsMain.TIMEOUT, JavaSortsMain.UNIT);
+        } catch (InterruptedException e) {
+            logException(name);
+            Thread.currentThread().interrupt();
+        } catch (ExecutionException | TimeoutException e) {
+            logException(name);
+        } finally {
+            executor.shutdown();
+        }
+
+        logElapsedTime(name, listCopy.size(), outputSb.toString());
+    }
+
+    /**
+     * Logs the elapsed time for a sorting operation on a list.
+     *
+     * @param name      the algorithm name used for sorting
+     * @param listSize  the size of the list being sorted
+     * @param output    the output containing the formatted duration of the sorting operation
+     */
+    private static void logElapsedTime(String name, int listSize, String output) {
+        String formattedDuration = extractTimeFromOutput(output);
+
+        if (!"Time not available".equals(formattedDuration)) {
+            LOGGER.log(Level.INFO, "Sorting with {0} algorithm on list of size {1} took {2}",
+                    new Object[]{name, listSize, formattedDuration});
+            DB_LOGGER.addLog(formattedDuration, listSize, name);
+        }
+    }
+
+    /**
+     * Handles the completion of a list of Future tasks.
+     *
+     * @param futures the list of Future tasks to handle completion
+     */
+    private static void handleFutures(@NotNull List<Future<?>> futures) {
         for (Future<?> future : futures) {
             try {
                 future.get(TIMEOUT, UNIT);
             } catch (InterruptedException e) {
+                logException("Generic task");
                 Thread.currentThread().interrupt();
-                LOGGER.log(Level.SEVERE, "Task execution was interrupted or failed.", e);
-            } catch (ExecutionException e) {
-                LOGGER.log(Level.SEVERE, "Task execution was interrupted or failed.", e);
-            } catch (TimeoutException e) {
-                LOGGER.log(Level.SEVERE, "Task execution exceeded the time limit.", e);
+            } catch (ExecutionException | TimeoutException e) {
+                logException("Generic task");
             }
         }
+    }
+
+    /**
+     * Logs an exception with an appropriate message.
+     *
+     * @param taskName the name of the task that was interrupted or failed
+     */
+    private static void logException(String taskName) {
+        LOGGER.log(Level.SEVERE, "The execution of the {0} was interrupted or failed.", new Object[]{taskName});
     }
 }
