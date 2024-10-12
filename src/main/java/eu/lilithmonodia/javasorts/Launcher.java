7479dd01073da3b1e88f08f7e67e4d5f7a8b19ea
Lilith Camplin <pierre.camplin.etu@univ-lille.fr>
2024-10-12 18:07:38 +0100

    Added documentation to Launcher

--- src/main/java/eu/lilithmonodia/javasorts/Launcher.java
+++ src/main/java/eu/lilithmonodia/javasorts/Launcher.java
@@ -5,9 +5,17 @@ import org.jetbrains.annotations.NotNull;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * Launcher class of the project used to access every aspect of the project
+ */
 public class Launcher {
     private static final Logger LOGGER = Logger.getLogger(Launcher.class.getName());
 
+    /**
+     * The main method of the program used to access the two sides of the project
+     *
+     * @param args argument of the program
+     */
     public static void main(String @NotNull [] args) {
         if (args.length >= 1 && args[0].equals("--help")) {
             LOGGER.log(Level.INFO ,"Usage: java -jar <file-name>.jar --console or java -jar <file-name>.jar");
